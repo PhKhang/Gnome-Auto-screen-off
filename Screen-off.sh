2ba3c7c34a1a2f1cf1b069db99953633b8f12f1e
NotANekomancer <84757707+PhKhang@users.noreply.github.com>
2022-07-10 17:14:18 +0700

    Update Screen-off.sh

--- Screen-off.sh
+++ Screen-off.sh
@@ -2,10 +2,10 @@
 while true ; do 
     if [ "$(cat /proc/acpi/button/lid/LID/state)" = "state:      open" ];
     then 
-        xset -display :0.0 dpms force on
+        xset dpms force on
         #echo "opened"
     else 
-        xset -display :0.0 dpms force off 
+        xset dpms force off 
         #echo "closed"
     fi  
 done
